fe11d55460babe626ac5b8490e34caca3d1488ce
Christopher Lindblom <christopher@distancify.com>
2017-05-16 15:14:03 +0200

    renamed confusingly named project .Manual to .Debugger

--- Seq.App.Freshdesk.Debugger/Program.cs
+++ Seq.App.Freshdesk.Debugger/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Seq.App.Freshdesk.Manual
+namespace Seq.App.Freshdesk.Debugger
 {
     class Program
     {
